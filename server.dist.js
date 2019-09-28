e04baf88451ca71f730f556b97d8677163da0b9d
Daniel Zhang <zhang00710@sina.com>
2019-09-29 01:00:31 +0800

    bundle for distribution

---
+++ server.dist.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const path = require("path");
+
+const complements = [
+  "You look nice today",
+  "That dress looks nice on you",
+  "Have you been working out?",
+  "You can do hard things",
+  "You've gotten far in this course. You're really smart",
+  "You're programming! How cool is that?",
+  "I'm really proud of you",
+  "You made this",
+  "You've learned a lot of things, and that's pretty hard to do"
+];
+
+const insults = [
+  "You look bad everyday",
+  "You can't do anything",
+  "You never made it"
+]
+
+function getRandomInsult() {
+  const randomIndex = Math.floor(Math.random() * insults.length);
+  return insults[randomIndex];
+}
+
+function getRandomComplement() {
+    const randomIndex = Math.floor(Math.random() * complements.length);
+    return complements[randomIndex];
+}
+
+const app = express();
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname,"dist/index.html"));
+});
+
+app.get("/complement", function(req, res) {
+  res
+    .json({
+        complement: getRandomComplement()
+    })
+    .end();
+});
+
+app.get("/insult", function(req, res) {
+  res
+    .json({
+        insult: getRandomInsult()
+    })
+    .end();
+});
+
+app.use("/", express.static("./dist"));
+
+app.listen(3000);
+console.log("listening on http://localhost:3000");

ff8d5946cbeb23f66e65be4bee6a930d013e3630
Daniel Zhang <zhang00710@sina.com>
2019-09-28 23:44:20 +0800

    add animation for buttons using popmotion

--- public/complements.js
+++ public/complements.js
@@ -1,3 +1,5 @@
+//const popmotion = require('popmotion');
+
 document
   .querySelector(".request-complement")
   .addEventListener("click", function() {
@@ -27,3 +29,31 @@ document
         console.error(err);
       });
   });
+
+const { styler, spring, listen, pointer, value } = popmotion;
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(function (btn) {
+  const divStyler = styler(btn);
+  const btnXY = value({ x: 0, y: 0 }, divStyler.set);
+  listen(btn, 'mousedown touchstart')
+    .start((e) => {
+      e.preventDefault();
+      pointer(btnXY.get()).start(btnXY);
+    });
+  
+  listen(document, 'mouseup touchend')
+    .start(() => {
+      spring({
+        from: btnXY.get(),
+        velocity: btnXY.getVelocity(),
+        to: { x: 0, y: 0 },
+        stiffness: 200,
+        // mass: 1,
+        // damping: 10
+      }).start(btnXY);
+    });
+  })
+
+ 
